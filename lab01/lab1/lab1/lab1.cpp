749c0114a397cd2085eaee09dfadc505c6ee49eb
Arseniy Rauba <155465667+ArseniyRauba@users.noreply.github.com>
2025-01-28 17:25:03 +0300

    Add files via upload

---
+++ lab01/lab1/lab1/lab1.cpp
@@ -0,0 +1,64 @@
+﻿//#include <iostream>
+//#include<chrono>
+//using namespace std;
+//
+//int main()
+//{
+//	setlocale(LC_ALL, "rus");
+//
+//	long long num;
+//	long long  num0 = 0, num1 = 1, i = 0, N;
+//	float a, b;
+//
+//	cout << "Введите номер числа:";
+//	cin >> N;
+//
+//	a = clock();
+// 
+//	while (i < N)
+//	{
+//		num = num0 + num1;
+//		num0 = num1;
+//		num1 = num;
+//		i += 1;
+//	}
+//
+//	cout << "Число: " << num1 << endl;
+//	b = clock();
+//	cout << "Время: " << (b - a) / 1000;
+//}
+
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+unsigned long long number(int n)
+{
+	if (n < 2) return 1;
+	return number(n - 1) + number(n - 2);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	float a, b;
+	int N, i;
+
+	cout << "Введите номер числа:";
+	cin >> N;
+
+	a = clock();
+
+	for (i = 0; i <= N; i++)
+	{
+		cout << number(i) << " ";
+	}
+
+	cout << endl;
+
+	b = clock();
+	cout << "Время: " << (b - a) / 1000;
+
+	return 0;
+}
---
+++ lab02/lab02/lab02/lab02.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+
+	int X, N, choice, num;
+
+	cout << "enter a N number: ";
+	cin >> N;
+
+	if (N == 0) {
+		cout << "N не может быть равно 0!" << endl;
+		cout << "enter a N number: ";
+		cin >> N;
+	}
+
+	srand(time(NULL));
+	X = rand() % N+1;
+	cout << "number X = " << X << endl;
+
+	int startNum = 1, endNum=N+1, i=0;
+
+	while (true) {
+		i += 1;
+		num = (((endNum - startNum) / 2) + startNum);
+		cout << "Num = " << num << "?" << endl;
+		cout << "1 - Yes; 2 - More; 3 - Less." << endl;
+		cin >> choice;
+		switch (choice) {
+		case 1:
+			cout << "Awesome!" << endl;
+			cout << "Max number of steps: " << int((log(N) / log(2)) + 1) << endl;
+			cout << "Current number of steps: " << i;
+			exit(0);
+		case 2:
+			startNum = num;
+			break;
+		case 3 :
+			endNum = num;
+			break;
+		}
+	}
+}
